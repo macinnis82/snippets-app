090213d7776147a3831ca4e1245286b097dfdbf1
Ryan MacInnis <MacInnis82@gmail.com>
2015-07-23 01:40:24 +0000

    created subparser

--- snippets.py
+++ snippets.py
@@ -33,6 +33,13 @@ def main():
     """Main function"""
     logging.info("Constructing parser")
     parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+
     arguments = parser.parse_args(sys.argv[1:])
 
 if __name__ == "__main__":
