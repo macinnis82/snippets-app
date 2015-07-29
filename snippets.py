9d89ea600ff1bec52fc2b87305c5969ffa844e9f
Ryan MacInnis <MacInnis82@gmail.com>
2015-07-29 19:55:09 +0000

    Added hidden catalog/search functionality and hidden snippets

--- snippets.py
+++ snippets.py
@@ -9,29 +9,19 @@ logging.debug("Connecting to PostgreSQL")
 connection = psycopg2.connect("dbname='snippets' user='ubuntu' password='thinkful' host='localhost'")
 logging.debug("Database connection established")
 
-def put(name, snippet):
+def put(name, snippet, hide=False):
   """
     Store a snippet with an associated name.
     Returns the name and the snippet
   """
   logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
   
-  # cursor = connection.cursor()
-  # try:
-  #   command = "insert into snippets values (%s,%s)"
-  #   cursor.execute(command, (name, snippet))
-  # except psycopg2.IntegrityError as e:
-  #   connection.rollback()
-  #   command = "update snippets set message=%s where keyword=%s"
-  #   cursor.execute(command, (snippet, name))
-  # connection.commit()
-  
   with connection, connection.cursor() as cursor:
     try:
-      cursor.execute("insert into snippets values (%s,%s)", (name, snippet))
+      cursor.execute("insert into snippets values (%s,%s,%s)", (name, snippet, hide))
     except psycopg2.IntegrityError:
       connection.rollback()
-      cursor.execute("update snippets set message=%s where keyword=%s", (snippet, name))
+      cursor.execute("update snippets set message=%s, hidden=%s where keyword=%s", (snippet, name, hide))
       connection.commit()
   
   logging.debug("Snippet stored successfully.")
@@ -43,32 +33,58 @@ def get(snippet):
     If there is no such snippet...
     Returns the snippet.
   """
-  logging.debug("Retrieving snippet {!r}".format(snippet))
+  logging.info("Retrieving snippet {!r}".format(snippet))
   
-  # cursor = connection.cursor()
-  # command = "select keyword, message from snippets where keyword = %s"
-  # snippet_tuple = (snippet,)
-  # cursor.execute(command, snippet_tuple)
-  # row = cursor.fetchone()
-  # connection.commit()
-
   with connection, connection.cursor() as cursor:
     cursor.execute("select message from snippets where keyword=%s", (snippet,))
     row = cursor.fetchone()
 
+  if row: return row[0]
+  else: print "No row with this snippet: {}".format(snippet)
+  
   logging.debug("Snippet retrieved successfully")
-
-  if row:
-    return row[0]
-  else:
-    print "No row with this snippet: {}".format(snippet)
 
 def delete(name):
   """
     Retrieve the snippet with a given name
     Delete the snippet
   """
-  logging.error("FIXME: Unimplemented - delete({!r})".format(name))
+  logging.info("Deleting record from database.")
+  
+  with connection, connection.cursor() as cursor:
+    cursor.execute("delete from snippets where keyword=%s", (name,))
+    
+  logging.debug("Record deleted from database.")
+  return name
+  
+def catalog():
+  """
+    Retrieve all the keywords in the snippets db
+  """
+  logging.info("Retrieving all the keywords.")
+
+  with connection, connection.cursor() as cursor:
+    cursor.execute("select keyword from snippets where hidden=False order by keyword ASC")
+    rows = cursor.fetchall()
+  
+    for row in rows: 
+      print row[0]
+    
+  logging.debug("All keywords retrieved successfully")
+  
+def search(string):
+  """
+  """
+  logging.info("Searching a snippet")
+  
+  with connection, connection.cursor() as cursor:
+    cursor.execute("select * from snippets where message like '%%' ||%s|| '%% and hidden=False'", (string,))
+    rows = cursor.fetchall()
+    
+    for row in rows:
+      print row[0]
+      
+  logging.debug("Searching complete")
 
 def main():
   """Main function"""
@@ -82,11 +98,26 @@ def main():
   put_parser = subparsers.add_parser("put", help="Store a snippet")
   put_parser.add_argument("name", help="The name of the snippet")
   put_parser.add_argument("snippet", help="The snippet text")
+  put_parser.add_argument("--hide", help="Sets the hidden column to True", action="store_true")
 
   # Subparser for the get command
   logging.debug("Constructing get subparser")
   get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
-  get_parser.add_argument("snippet", help="The snipper text")
+  get_parser.add_argument("snippet", help="The snippet text")
+  
+  # Subparser for the catalog command
+  logging.debug("Constructing catalog subparser")
+  subparsers.add_parser("catalog", help="Retrieves all keywords in snippets")
+  
+  # Subparser for the search command
+  logging.debug("Constructing search subparser")
+  search_parser = subparsers.add_parser("search", help="Searches the database for specified string")
+  search_parser.add_argument("string", help="The string that will be used to search the database")
+  
+  # Subparser for the delete command
+  logging.debug("Constructing put subparser")
+  delete_parser = subparsers.add_parser("delete", help="Delete a snippet")
+  delete_parser.add_argument("name", help="The name of the snippet")
 
   arguments = parser.parse_args(sys.argv[1:])
 
@@ -100,6 +131,15 @@ def main():
   elif command == "get":
     snippet = get(**arguments)
     print("Retrieved snippet: {!r}".format(snippet))
+  elif command == 'catalog':
+    catalog()
+    print("Query finished successfully")
+  elif command == 'search':
+    search(**arguments)
+    print("Search complete for string: {!r}".format(sys.argv[2]))
+  elif command == 'delete':
+    delete(**arguments)
+    print("Deleted snippet: {!r}".format(sys.argv[2]))
 
 if __name__ == "__main__":
   main()
